33f2bc41f8c052a59ab983ee2624641cdde4da3a
alanVinco <alan.montiel.avi@gmail.com>
2024-05-07 11:30:16 -0600

    fin sección 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'basses';
+  public title: string = 'Hola world';
+  public counter: number = 10;
+
+  increaseBy( value:number):void{
+    this.counter +=value;
+  }
+
+  reset():void{
+    this.counter = 10
+  }
+
 }
